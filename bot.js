97a752452913a48fe49a3cc855e4457811869035
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-19 12:50:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -345,7 +345,7 @@ client.on('message', message => {
 
 
 
-const developers = ["472951521798127626","506832717137182733"]
+const developers = ["445406863048048642","417377495160193044"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
